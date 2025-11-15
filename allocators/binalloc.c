33b951c8a946692d634186b764f547c73bc1c397
nikolaj <fotikolay222@gmail.com>
2025-11-16 00:21:25 +0500

    allocaters separated to implementation and header

---
+++ allocators/binalloc.c
@@ -0,0 +1,97 @@
+#include <binalloc.h>
+
+static unsigned char _buffer[ALLOC_BUFFER_SIZE] = { 0 };
+static mm_bblock_t* _mm_head = (mm_bblock_t*)_buffer;
+static mm_info_t _mm_info = { .allocated = 0 };
+
+static int __block_init(mm_bblock_t* block, unsigned int size) {
+    block->magic = BLOCK_MAGIC;
+    block->size  = size;
+    block->free  = 1;
+    block->l = NULL;
+    block->r = NULL;
+    return 1;
+}
+
+int b_init() {
+    if (_mm_head->magic != BLOCK_MAGIC) {
+        __block_init(_mm_head, ALLOC_BUFFER_SIZE - sizeof(mm_bblock_t));
+    }
+    return 1;
+}
+
+static int __b_coalesce_memory(mm_bblock_t* block) {
+    if (!block || !block->free) return 0;
+    if (!block->l || !block->r) return 0;
+
+    if (block->l->free && block->r->free && 
+        !block->l->l && !block->l->r && 
+        !block->r->l && !block->r->r) {
+        block->l = NULL;
+        block->r = NULL;
+        return 1;
+    }
+
+    return 0;
+}
+
+static void* __b_malloc(unsigned int size, mm_bblock_t* block) {
+    if (!block || !block->free) return NULL;
+    if (block->size < size) return NULL;
+
+    unsigned int half_size = block->size / 2;
+    if (half_size < sizeof(mm_bblock_t) + MIN_BLOCK || 
+        half_size - sizeof(mm_bblock_t) < size) {
+        block->free = 0;
+        return (unsigned char*)block + sizeof(mm_bblock_t);
+    }
+
+    if (!block->l && !block->r) {
+        block->l = (mm_bblock_t*)((unsigned char*)block + sizeof(mm_bblock_t));
+        __block_init(block->l, half_size - sizeof(mm_bblock_t));
+
+        block->r = (mm_bblock_t*)((unsigned char*)block->l + half_size);
+        __block_init(block->r, half_size - sizeof(mm_bblock_t));
+    }
+
+    void* res = __b_malloc(size, block->l);
+    if (res) return res;
+
+    res = __b_malloc(size, block->r);
+    if (res) return res;
+
+    if (__b_coalesce_memory(block)) 
+        return __b_malloc(size, block);
+        
+    return NULL;
+}
+
+void* b_malloc(unsigned int size) {
+    if (!size) return NULL;
+    size = (size + ALIGNMENT - 1) & ~(ALIGNMENT - 1);
+    void* ptr = __b_malloc(size, _mm_head);
+    if (ptr) {
+        mm_bblock_t* block = (mm_bblock_t*)((unsigned char*)ptr - sizeof(mm_bblock_t));
+        _mm_info.allocated += block->size;
+    }
+    
+    return ptr;
+}
+
+int b_free(void* ptr) {
+    if (!ptr || ptr < (void*)_buffer || ptr >= (void*)(_buffer + ALLOC_BUFFER_SIZE)) 
+        return 0;
+        
+    mm_bblock_t* block = (mm_bblock_t*)((unsigned char*)ptr - sizeof(mm_bblock_t));
+    if (block->magic != BLOCK_MAGIC) return 0;
+    if (block->free) return 0;
+    
+    block->free = 1;
+    _mm_info.allocated -= block->size;
+    return 1;
+}
+
+int b_info(mm_info_t* i) {
+    i->allocated = _mm_info.allocated;
+    return 1;
+}
--- allocators/llalloc.h
+++ allocators/llalloc.h
@@ -25,106 +25,11 @@ typedef struct mm_llblock {
     struct mm_llblock* next;
 } mm_llblock_t;
 
-static unsigned char _buffer[ALLOC_BUFFER_SIZE] = { 0 };
-static mm_llblock_t* _mm_head = (mm_llblock_t*)_buffer;
-static mm_info_t _mm_info = { .allocated = 0 };
-
-int ll_init() {
-    if (_mm_head->magic != BLOCK_MAGIC) {
-        _mm_head->magic = BLOCK_MAGIC;
-        _mm_head->size  = ALLOC_BUFFER_SIZE - sizeof(mm_llblock_t);
-        _mm_head->free  = 1;
-        _mm_head->next  = NULL;
-    }
-    
-    return 1;
-}
-
-static int __ll_coalesce_memory() {
-    int merged = 0;
-    mm_llblock_t* current = _mm_head;
-    
-    do {
-        merged = 0;
-        current = _mm_head;
-
-        while (current && current->magic == BLOCK_MAGIC && current->next) {
-            if (current->free && current->next->free) {
-                current->size += sizeof(mm_llblock_t) + current->next->size;
-                current->next = current->next->next;
-                merged = 1;
-            } 
-            else {
-                current = current->next;
-            }
-        }
-    } while (merged);
-    return 1;
-}
-
-static void* __ll_malloc(unsigned int size, unsigned int offset, int prepare_mem) {
-    if (!size) return NULL;
-    if (prepare_mem) __ll_coalesce_memory();
-
-    size   = (size + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1);
-    offset = (offset + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1);
-    mm_llblock_t* current = _mm_head;
-    
-    while (current && current->magic == BLOCK_MAGIC) {
-        void* aligned_addr = (unsigned char*)current + sizeof(mm_llblock_t);
-        unsigned int position = (unsigned int)((unsigned char*)aligned_addr - _buffer);
-        if (current->free && current->size >= size && position >= offset) {
-            if (current->size >= size + sizeof(mm_llblock_t)) {
-                mm_llblock_t* new_block = (mm_llblock_t*)((unsigned char*)current + sizeof(mm_llblock_t) + size);
-                new_block->magic = BLOCK_MAGIC;
-                new_block->size = current->size - size - sizeof(mm_llblock_t);
-                new_block->free = 1;
-                new_block->next = current->next;
-
-                current->next = new_block;
-                current->size = size;
-            }
-
-            current->free = 0;
-            return (unsigned char*)current + sizeof(mm_llblock_t);
-        }
-
-        current = current->next;
-    }
-
-    return prepare_mem ? NULL : __ll_malloc(size, offset, 1);
-}
-
-void* ll_malloc(unsigned int size) {
-    void* ptr = __ll_malloc(size, NO_OFFSET, 0);
-    if (ptr) _mm_info.allocated += size;
-    return ptr;
-}
-
-void* ll_mallocoff(unsigned int size, unsigned int offset) {
-    void* ptr = __ll_malloc(size, offset, 0);
-    if (ptr) _mm_info.allocated += size;
-    return ptr;
-}
-
-int ll_free(void* ptr) {
-    if (!ptr || ptr < (void*)_buffer || ptr >= (void*)(_buffer + ALLOC_BUFFER_SIZE)) {
-        return 0;
-    }
-    
-    mm_llblock_t* block = (mm_llblock_t*)((unsigned char*)ptr - sizeof(mm_llblock_t));
-    if (block->magic != BLOCK_MAGIC) return 0;
-    if (block->free) return 0;
-
-    block->free = 1;
-    _mm_info.allocated -= block->size;
-    return 1;
-}
-
-static int ll_info(mm_info_t* i) {
-    i->allocated = _mm_info.allocated;
-    return 1;
-}
+int ll_init();
+void* ll_malloc(unsigned int size);
+void* ll_mallocoff(unsigned int size, unsigned int offset);
+int ll_free(void* ptr);
+int ll_info(mm_info_t* i);
 
 #ifdef __cplusplus
 }
